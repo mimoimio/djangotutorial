1ade55a36b59acb2c0db37018db8c40c69b0fa9f
mimoimio <129141433+mimoimio@users.noreply.github.com>
2025-05-10 17:43:14 +0800

    completed tutorial part 4

--- mysite/views.py
+++ mysite/views.py
@@ -1,4 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the / index. lol")
+    return render(request, "index.html")
+    # return HttpResponse("Hello, world. You're at the / index. lol")
+    
+def about(request):
+    return render(request, "about.html")
